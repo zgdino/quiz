6034f189fc23a12da2fc2f57b127fc26840ec248
Dino Hadzic <zgdino@yahoo.com>
2022-05-31 17:05:30 +0200

    context initial setup

--- setup/src/context.js
+++ setup/src/context.js
@@ -14,9 +14,31 @@ const tempUrl =
   'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 
 const AppContext = React.createContext()
+// if waiting display the form, if not, display the questions
+const [waiting, setWaiting] = useState(true)
+const [laoding, setLoading] = useState(false)
+const [questions, setQuestions] = useState([])
+const [index, setIndex] = useState(0)
+const [correct, setCorrect] = useState(0)
+const [error, setError] = useState(false)
+const [isModalOpen, setIsModalOpen] = useState(false)
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        laoding,
+        questions,
+        index,
+        correct,
+        error,
+        isModalOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
 }
 // make sure use
 export const useGlobalContext = () => {
