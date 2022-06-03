831fb0daf4ada8c1903c16f163749c842f1aea58
Dino Hadzic <zgdino@yahoo.com>
2022-06-03 18:56:15 +0200

    next question

--- setup/src/App.js
+++ setup/src/App.js
@@ -5,7 +5,7 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
-  const { questions, loading, waiting, index, correct } = useGlobalContext()
+  const { questions, loading, waiting, index, correct, nextQuestion } = useGlobalContext()
   if (waiting) {
     return <SetupForm />
   }
@@ -39,7 +39,7 @@ function App() {
             })}
           </div>
         </article>
-        <button className='next-question'>next question</button>
+        <button className='next-question' onClick={nextQuestion}>next question</button>
       </section>
     </main>
   )
