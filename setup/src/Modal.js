a40d729ac533de03f7d32618b33d7378965e6ac3
Dino Hadzic <zgdino@yahoo.com>
2022-06-05 05:13:56 +1200

    modal basic setup

--- setup/src/Modal.js
+++ setup/src/Modal.js
@@ -2,12 +2,20 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Modal = () => {
-  const {isModalOpen, closeModal, correct, questions} = useGlobalContext()
-  return <div className={`${isModalOpen ? 'modal-container isOpen' : 'modal-container'}`}>
-    <div className="modal-content">
-      <h2>congrats</h2>
+  const { isModalOpen, closeModal, correct, questions } = useGlobalContext()
+  return (
+    <div
+      className={`${
+        isModalOpen ? 'modal-container isOpen' : 'modal-container'
+      }`}
+    >
+      <div className='modal-content'>
+        <h2>congrats</h2>
+        <p>You answered of questions correctly</p>
+        <button className='close-btn' onClick={closeModal}>play again</button>
+      </div>
     </div>
-  </div>
+  )
 }
 
 export default Modal
