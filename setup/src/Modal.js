5ba183c0e7b3c3e465e3929b4730a0b944d6de09
Dino Hadzic <zgdino@yahoo.com>
2022-06-05 05:19:06 +1200

    modal message

--- setup/src/Modal.js
+++ setup/src/Modal.js
@@ -11,7 +11,7 @@ const Modal = () => {
     >
       <div className='modal-content'>
         <h2>congrats</h2>
-        <p>You answered of questions correctly</p>
+        <p>You answered {((correct/questions.length)*100).toFixed(0)}% of questions correctly</p>
         <button className='close-btn' onClick={closeModal}>play again</button>
       </div>
     </div>
