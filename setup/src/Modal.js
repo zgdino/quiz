7e1696011aa154a1ad0516756444fdab90f77867
Dino Hadzic <zgdino@yahoo.com>
2022-06-06 21:38:34 +1200

    m

--- setup/src/Modal.js
+++ setup/src/Modal.js
@@ -5,6 +5,7 @@ const Modal = () => {
   const { isModalOpen, closeModal, correct, questions } = useGlobalContext()
   return (
     <div
+    // changing className based on weather the modal is open or not
       className={`${
         isModalOpen ? 'modal-container isOpen' : 'modal-container'
       }`}
@@ -12,6 +13,7 @@ const Modal = () => {
       <div className='modal-content'>
         <h2>congrats</h2>
         <p>You answered {((correct/questions.length)*100).toFixed(0)}% of questions correctly</p>
+        {/* onClick close the modal and show setup form */}
         <button className='close-btn' onClick={closeModal}>play again</button>
       </div>
     </div>
